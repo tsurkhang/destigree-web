34c469cd273cf31b46eef99bfda1a6e1185a2844
Tara <tsurkhang@smls.on.ca>
2026-01-10 15:41:21 -0500

    idk

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { DegreeResponse } from '../models/degree.interface';
 import { DegreeService } from '../degree.service';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, FormsModule,MatFormFieldModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
