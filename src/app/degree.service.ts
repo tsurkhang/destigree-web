bc97d40ef254fec2c7ec1f6b26b38228753cb38e
Tara <tsurkhang@smls.on.ca>
2026-01-10 14:38:29 -0500

    home component + degree service

---
+++ src/app/degree.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DegreeResponse } from './models/degree.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DegreeService {
+
+  constructor() { }
+}
