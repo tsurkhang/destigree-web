c1d0a47a6632ac16c74eaea9528b625c316c74d7
grace <gracevjiang@gmail.com>
2026-01-10 16:19:36 -0500

    html stuff test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatInputModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
